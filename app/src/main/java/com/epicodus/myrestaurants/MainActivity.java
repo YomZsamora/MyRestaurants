d5e254957551618cfd009c44fae9be03dc5b3c25
yomz <samora.y@adzumi.co.ke>
2019-02-28 07:25:23 +0300

    Creating a GridView with a Custom Layout.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -7,28 +7,33 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
 
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onClick(View v){
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/epicodus/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,30 @@
+package com.epicodus.myrestaurants;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.length;
+    }
+
+}
